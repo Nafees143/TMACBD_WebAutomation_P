421e3bd2e4d0d17da7d12623b44be9c0fb757034
Nafees413 <menafees143@gmail.com>
2022-03-13 15:36:03 +0600

    navigation & Contact form fill-up

--- src/test/java/model/Locators.java
+++ src/test/java/model/Locators.java
@@ -7,9 +7,23 @@ public class Locators {
 	public static String homePageUrl = "https://tmacbd.com/";
 	
 	
-	//landing page
+	//logo click
 	public static By LogoClick = By.xpath("//img[@src='assets/images/logo.png']");
 	
-	//contact form field
+	//navigation menus
+	public static By HomeClick = By.xpath("//a[contains(text(),'Home')]");
+	public static By AboutClick = By.xpath("//a[contains(text(),'About')]");
+	public static By ServicesClick = By.xpath("//a[contains(text(),'Services')]");
+	public static By TeamClick = By.xpath("//a[contains(text(),'Team')]");
+	public static By ProjectsClick = By.xpath("//a[contains(text(),'Projects')]");
+	public static By ContactUsClick = By.xpath("//a[contains(text(),'Contact Us')]");
+	
+	
+	//contact US form field
+	public static By EnterName = By.xpath("//input[@id='username']");
+	public static By EnterEmil = By.xpath("//input[@id='email']");
+	public static By EnterSubject = By.xpath("//input[@id='subject']");
+	public static By EnterMessage = By.xpath("//textarea[@id='message']");
+	public static By SendMessageBtn = By.xpath("//button[@id='submit']");
 	
 }
--- src/test/java/testCases/TestCases.java
+++ src/test/java/testCases/TestCases.java
@@ -27,8 +27,45 @@ public class TestCases {
 	}
 	@Test
 	public void landingPage() throws InterruptedException {
+		
+		//navigation bar
 		commonMethods.clickBtn(Locators.LogoClick);
 		Thread.sleep(3000);
+		
+		commonMethods.clickBtn(Locators.HomeClick);
+		Thread.sleep(2000);
+		
+		commonMethods.clickBtn(Locators.AboutClick);
+		Thread.sleep(2000);
+		
+		commonMethods.clickBtn(Locators.ServicesClick);
+		Thread.sleep(2000);
+		
+		commonMethods.clickBtn(Locators.TeamClick);
+		Thread.sleep(2000);
+		
+		commonMethods.clickBtn(Locators.ProjectsClick);
+		Thread.sleep(2000);
+		
+		commonMethods.clickBtn(Locators.ContactUsClick);
+		Thread.sleep(2000);
+		
+		
+		//contact form field
+		commonMethods.sendText(Locators.EnterName, "Tester");
+		Thread.sleep(2000);
+		
+		commonMethods.sendText(Locators.EnterEmil, "Tester@gmail.com");
+		Thread.sleep(2000);
+		
+		commonMethods.sendText(Locators.EnterSubject, "Muibornafriha");
+		Thread.sleep(2000);
+		
+		commonMethods.sendText(Locators.EnterMessage, "zahid the name of play boy muib also");
+		Thread.sleep(2000);
+		
+		commonMethods.clickBtn(Locators.SendMessageBtn);
+		Thread.sleep(2000);
 		
 		base.tearDown();
 	}
