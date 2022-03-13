eaff97a7e5ea0bbe0c76566ce8c4275a83deef16
Nafees413 <menafees143@gmail.com>
2022-03-13 11:28:22 +0600

    first commit

---
+++ src/test/java/model/CommonMethods.java
@@ -0,0 +1,12 @@
+package model;
+
+import org.openqa.selenium.WebDriver;
+
+public class CommonMethods {
+
+	WebDriver driver;
+	
+	public CommonMethods(WebDriver driver) {
+		this.driver = driver;
+	}
+}
