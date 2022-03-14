2545deccc5636579f1b713af9d89d30c9566aa04
Nafees413 <menafees143@gmail.com>
2022-03-14 16:06:10 +0600

    scroll done

--- src/test/java/testCases/TestCases.java
+++ src/test/java/testCases/TestCases.java
@@ -1,9 +1,7 @@
 package testCases;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
